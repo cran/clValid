c1d5b57da826a64b1c5974a947d8a50617bb1c0c
Guy Brock <guy.brock@louisville.edu>
2007-03-29 00:00:00 +0000

    version 0.5-1

---
+++ R/zzz.r
@@ -0,0 +1,6 @@
+## startup file for clValid package
+#.First.lib <- function(libname, pkgname, where) {
+#  if( !require(methods) ) stop("we require methods for package clValid")
+#  where <- match(paste("package:", pkgname, sep=""), search())
+#  .initClValid(where)
+#}
